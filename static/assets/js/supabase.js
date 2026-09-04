db7aa87de6b0196d8ccad61eef20e79a4f3bd240
Leaoz1n <164673541+Leaoz1n@users.noreply.github.com>
2026-09-04 20:13:43 -0300

    Delete static directory

--- static/assets/js/supabase.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://lnureygnpxpunaoghktq.supabase.co";
-const SUPABASE_KEY = "sb_publishable_x-D-sOTVKr0LMtLTA4TRVA_thGwl2nR";
-
-const supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
